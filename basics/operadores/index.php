4ea510fe0ee9064389da013c77e36632d9f95bfb
Katia Reis <62106987+katiareis@users.noreply.github.com>
2022-03-28 10:30:55 -0300

    Arithmetic operators
    Arithmetic operators in php.

---
+++ basics/operadores/index.php
@@ -0,0 +1,18 @@
+<?php
+  /* Operados Aritméticos
+
+  Adição +
+  Subtração -
+  Multiplicação *
+  Divisão /
+  Módulo %
+
+  */
+
+  $n1 = 2;
+  $n2 = 3;
+  $soma = $n1 + $n2;
+
+  echo "A some vale $soma";
+
+?>
---
+++ basics/operadores/noobcalc.php
@@ -0,0 +1,11 @@
+<?php 
+  /* Noob Calc */
+
+  $n1 = 2;
+  $n2 = 3;
+  $soma = $n1 + $n2;
+
+  
+
+  echo "A some vale $soma";
+?>
